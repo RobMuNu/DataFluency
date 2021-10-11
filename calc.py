4a21ba25fcd7b6f7428d71aa362f320e02c903af
Roberto Munoz <roberto.munoz@Monash.edu>
2021-10-11 11:58:58 +1100

    Add support for multiplication

--- calc.py
+++ calc.py
@@ -7,6 +7,8 @@ def compute(expression):
         return num0 + num1
     elif operator == '-':
         return num0 - num1
+    elif operator == '*':
+    	return num0 * num1
     else:
         print('unknown operator!')
         return None
