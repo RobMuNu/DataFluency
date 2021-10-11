8be7d68ac0801b6d651449b775e74fd27f62e369
Ash-Wild <62725019+Ash-Wild@users.noreply.github.com>
2021-10-11 13:02:58 +1100

    Float values for integer 2

--- calc.py
+++ calc.py
@@ -3,7 +3,7 @@
 # Hello world
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
-    num0, num1 = float(num0), int(num1)
+    num0, num1 = float(num0), float(num1)
     if operator == '+':
         return num0 + num1
     elif operator == '-':
