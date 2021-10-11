9a4e637b1cfe0de39d438834f7e954fb745ca950
Roberto Munoz <roberto.munoz@Monash.edu>
2021-10-11 11:33:35 +1100

    use iterative unpacking functionality

--- calc.py
+++ calc.py
@@ -1,12 +1,9 @@
-def compute(expression):	
-	values = expression.split(' ')
-    num0 = int(values[0])
-    operator = values[1]
-    num1 = int(values[2])
+def compute(expression):
+    num0, operator, num1 = expression.split(' ')
     if operator == '+':
         return num0 + num1
     elif operator == '-':
-    	return num0 - num1
+        return num0 - num1
     else:
         print('unknown operator!')
         return None
