f34754327503e90cad9c264c01f97e3205605081
Roberto Munoz <roberto.munoz@Monash.edu>
2021-10-11 11:02:47 +1100

    initial work on python string calculator

---
+++ calc.py
@@ -0,0 +1,10 @@
+values = expression.split(' ')
+    num0 = int(values[0])
+    operator = values[1]
+    num1 = int(values[2])
+    if operator == '+':
+        return num0 + num1
+    else:
+        print('unknown operator!')
+        return 0
+     
