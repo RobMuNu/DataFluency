ad25fc4c83edea40e73479d8ab8d6b5fda62beda
Roberto Munoz <roberto.munoz@Monash.edu>
2021-10-11 11:12:13 +1100

    Add support for subtraction

--- calc.py
+++ calc.py
@@ -1,9 +1,12 @@
-values = expression.split(' ')
+def compute(expression):	
+	values = expression.split(' ')
     num0 = int(values[0])
     operator = values[1]
     num1 = int(values[2])
     if operator == '+':
         return num0 + num1
+    elif operator == '-':
+    	return num0 - num1
     else:
         print('unknown operator!')
         return None
